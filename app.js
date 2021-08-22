14d56b0d4763ca329aaf2396e68632d9cdde3839
Tom Tobar <tomas.tobar@flatironschool.com>
2021-08-21 23:03:04 -0500

    listen on port 4000

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use('/projects', projectsRoutes)
 
 
 //LISTEN ON PORT 3000
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+app.listen(4000, () => {
+    console.log('listening on port 4000')
 })
 
 // CONNTECT TO MONGODB
