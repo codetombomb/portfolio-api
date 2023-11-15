81a061c19b025401a71c29dd48048771ace14a6b
Tom Tobar <codetombomb@gmail.com>
2023-11-15 15:39:35 -0600

    add models and seeds

--- server/models.py
+++ server/models.py
@@ -11,6 +11,8 @@ class Admin(db.Model, SerializerMixin):
     last_name = db.Column(db.String)
     email = db.Column(db.String)
 
+    chats = db.relationship("Chat", backref="admins")
+
     _password_hash = db.Column(db.String)
     
     @hybrid_property
@@ -28,7 +30,7 @@ class Admin(db.Model, SerializerMixin):
         return bcrypt.check_password_hash(self._password_hash, provided_password)
 
     def __repr__(self):
-        return f"\n<Admin id={self.id},\nfirst_name={self.first_name},\nlast_name={self.last_name}\nemail={self.email}\n>"
+        return f"\n<Admin id={self.id},\n\tfirst_name={self.first_name},\n\tlast_name={self.last_name}\n\temail={self.email}\n>"
     
 class Chat(db.Model, SerializerMixin):
     __tablename__ = "chats"
@@ -43,7 +45,7 @@ class Chat(db.Model, SerializerMixin):
     messages = db.relationship("Message", backref="chats")
 
     def __repr__(self):
-        return f"\n<Chat id={self.id},\nadmin_id={self.admin_id},\nvisitor_id={self.visitor_id} >"
+        return f"\n<Chat id={self.id},\n\tcreated_at={self.created_at},\n\tadmin_id={self.admin_id},\n\tvisitor_id={self.visitor_id} \n>"
     
 
 class Message(db.Model, SerializerMixin):
@@ -55,15 +57,16 @@ class Message(db.Model, SerializerMixin):
 
     chat_id = db.Column(db.Integer, db.ForeignKey("chats.id"))
 
-    serialize_rules = ("message.chats")
+    sender_type = db.Column(db.String)
 
+    admin_id = db.Column(db.Integer, db.ForeignKey("admins.id"), nullable=True)
+    admin = db.relationship("Admin", foreign_keys=[admin_id], backref="admin_messages")
 
+    visitor_id = db.Column(db.Integer, db.ForeignKey("visitors.id"), nullable=True)
+    visitor = db.relationship("Visitor", foreign_keys=[visitor_id], backref="visitor_messages")
 
-    
-
-
-
-
+    def __repr__(self):
+        return f"\n<Message id={self.id},\n\tcreated_at={self.created_at},\n\tcontent={self.content},\n\tchat_id={self.chat_id},\n\tsender_type={self.sender_type},\n\tadmin_id={self.admin_id},\n\tvisitor_id={self.visitor_id}\n>"
 
 
 class Visitor(db.Model, SerializerMixin):
@@ -74,8 +77,10 @@ class Visitor(db.Model, SerializerMixin):
     last_name = db.Column(db.String)
     email = db.Column(db.String)
 
+    chats = db.relationship("Chat", backref="visitors")
+
     def __repr__(self):
-        return f"\n<User id={self.id},\nfirst_name={self.first_name},\nlast_name={self.last_name}\nemail={self.email}\n>"
+        return f"\n<User id={self.id},\n\tfirst_name={self.first_name},\n\tlast_name={self.last_name}\n\temail={self.email}\n>"
     
 
     
---
+++ server/seed.py
@@ -0,0 +1,108 @@
+from ipdb import set_trace
+from faker import Faker
+import random
+
+from config import app, db
+from models import Visitor, Admin, Chat, Message
+
+fake = Faker()
+
+with app.app_context():
+    
+    def clear_entries():
+        db.drop_all()
+        db.create_all()
+        # Admin.query.delete()
+        # Chat.query.delete()
+        # Message.query.delete()
+        # Visitor.query.delete()
+
+    def create_admin():
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        new_admin = Admin(
+            first_name=first_name,
+            last_name=last_name,
+            email=f"{first_name.lower()}.{last_name.lower()}@gmail.com"
+        )
+        db.session.add(new_admin)
+        db.session.commit()
+        return new_admin
+
+    def create_visitor():
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        new_visitor = Visitor(
+            first_name=first_name,
+            last_name=last_name,
+            email=f"{first_name.lower()}.{last_name.lower()}@gmail.com"
+        )
+        db.session.add(new_visitor)
+        db.session.commit()
+        return new_visitor
+    
+    def create_chat(admin_id, visitor_id):
+        new_chat = Chat(
+            admin_id=admin_id,
+            visitor_id=visitor_id
+        )
+
+        db.session.add(new_chat)
+        db.session.commit()
+        return new_chat
+    
+    def create_message(content, sender_type, chat_id, visitor_id=None, admin_id=None):
+        message = Message(
+            content=content,
+            sender_type=sender_type,
+            chat_id=chat_id,
+            visitor_id=visitor_id,
+            admin_id=admin_id,
+        )
+        db.session.add(message)
+        db.session.commit()
+        return message
+
+    def seed_db():
+        print("Seeding 🌱")
+        clear_entries()
+        for _ in range(5):
+            admin = create_admin() 
+            visitor = create_visitor()
+            new_chat = create_chat(admin.id,visitor.id)
+            
+            for _ in range(5):
+                create_message(
+                    fake.paragraph(nb_sentences=random.randint(0, 3)), 
+                    "admin", 
+                    new_chat.id,
+                    admin_id=admin.id, 
+                )
+
+            # content=content,
+            # sender_type=sender_type,
+            # chat_id=chat_id,
+            # visitor_id=visitor_id,
+            # admin_id=admin_id,
+
+                create_message(
+                    fake.paragraph(nb_sentences=random.randint(0, 3)),
+                    "visitor",
+                    new_chat.id,
+                    visitor.id
+                )
+
+
+        
+        print("Done seeding ✅")
+
+    seed_db()
+        
+
+
+
+       
+
+        
+    
+
